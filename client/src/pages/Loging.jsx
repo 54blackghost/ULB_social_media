5441a6b6bc17c29a6db84a73b3ecc10456d3b85d
54blackghost <ulrichleblack@gmail.com>
2026-04-29 00:32:26 +0200

    ajout de la barre de stories et du modal pour ajouter une story

--- client/src/pages/Loging.jsx
+++ client/src/pages/Loging.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {assets} from '../assets/assets'
 import { Star } from "lucide-react";
+import { SignIn } from '@clerk/react'
 
 
 const Login = () => {
@@ -37,7 +38,10 @@ const Login = () => {
             <span className="md:h-10"></span>
           </div>
 
-          {/* right side  : Login*/}
+          {/* right side  : Login Form*/}
+          <div className="flex-1 flex items-center justify-center p-6 sm:p-10">
+                <SignIn/>
+          </div>
       </div>
     )
 }
