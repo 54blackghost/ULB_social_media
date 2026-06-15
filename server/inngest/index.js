141b16f06456b5515d6f827f700cf893473e58c8
54blackghost <ulrichleblack@gmail.com>
2026-06-15 23:06:58 +0200

    developpement du backend

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,7 +1,21 @@
 import { Inngest } from "inngest";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "my-app" });
+export const inngest = new Inngest({ id: "pingup-app" });
+
+//Inngest Function to save user data to a database
+const syncUserCreation = inngest.createFunction(
+    {id: 'sync-user-from-clerk'},
+    {event: 'clerk/user.created'},
+    async ({event})=>{
+        const {id, first_name, last_name, email_addresses, image_url} = event.data
+        let username = email_addresses[0].email_address.split('@')[0]
+
+
+        //ckeck availability of username
+        const user = await User
+    }
+)
 
 // Create an empty array where we'll export future Inngest functions
 export const functions = [];
