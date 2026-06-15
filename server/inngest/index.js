f0d00cfbb0c68244f2d1da1eef43fa124cf51917
54blackghost <ulrichleblack@gmail.com>
2026-06-15 23:36:13 +0200

    developpement du backend

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,4 +1,5 @@
 import { Inngest } from "inngest";
+import User from '../models/User.js';
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "pingup-app" });
@@ -13,9 +14,57 @@ const syncUserCreation = inngest.createFunction(
 
 
         //ckeck availability of username
-        const user = await User
+        const user = await User.findOne({ username });
+
+        if(user){
+            username = username + Math.floor(Math.random() * 1000);
+        }  
+
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            full_name: first_name + ' ' + last_name,
+            profile_picture: image_url,
+            username
+        }// Save the user data to the database
+        await User.create(userData);
     }
 )
 
+
+//Inngest Function to update user data in the database
+const syncUserUpdate = inngest.createFunction(
+    {id: 'update-user-from-clerk'},
+    {event: 'clerk/user.updated'},
+    async ({event})=>{
+        const {id, first_name, last_name, email_addresses, image_url} = event.data
+
+    const updateUserData = {
+        email: email_addresses[0].email_address,
+        full_name: first_name + ' ' + last_name,
+        profile_picture: image_url, 
+    }
+    // Update the user data in the database
+    await User.findByIdAndUpdate(id, updateUserData);
+  }
+)
+
+
+//Inngest Function to delete user from database
+const syncUserDeletion = inngest.createFunction(
+    {id: 'delete-user-from-clerk'},
+    {event: 'clerk/user.deleted'},
+    async ({event})=>{
+        const {id} = event.data
+        // Delete the user from the database
+        await User.findByIdAndDelete(id);
+    }
+)
+
+
+
 // Create an empty array where we'll export future Inngest functions
-export const functions = [];
+export const functions = [
+    syncUserCreation, 
+    syncUserUpdate,
+    syncUserDeletion];
