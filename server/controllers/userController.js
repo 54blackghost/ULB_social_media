2b6f34a7720b52559b80deeae836cb28b9bce34d
54blackghost <ulrichleblack@gmail.com>
2026-06-16 01:36:55 +0200

    backend development

---
+++ server/controllers/userController.js
@@ -0,0 +1,47 @@
+import User from "../models/User";
+
+
+// Get User Data using userId
+export const getUserData = async (req, res) => {
+    try {
+        const {userId} = await req.auth();
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.json({success: false, message: 'User not found'});
+        }
+        res.json({success: true, data: user});
+    } catch (error) {
+        console.error(error);
+        res.json({success: false, message: error.message});
+    }
+}
+
+
+// Update User Data 
+export const updateUserData = async (req, res) => {
+    try {
+        const {userId} = await req.auth();
+        const {username, bio, location, full_name} = req.body;
+        const tempUser = await User.findById(userId);
+
+        !username && (username = tempUser.username);
+
+         if(tempUser.username !== username){
+            const user = User.findOne({username});
+            if(user){
+                //we will not change the username if it is already taken
+                username = tempUser.username;
+            }
+        }
+
+        const updateData ={
+            username,
+            bio,
+            location,
+            full_name
+        };
+    } catch (error) {
+        console.error(error);
+        res.json({success: false, message: error.message});
+    }
+}
