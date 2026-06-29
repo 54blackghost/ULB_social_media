f217e5806c25aa8d25dc6f191049e55b6a5bbdfb
54blackghost <ulrichleblack@gmail.com>
2026-06-29 16:23:58 +0200

    suivie du backend

--- server/configs/imagesKit.js
+++ server/configs/imagesKit.js
@@ -1,16 +1,12 @@
 import ImageKit from '@imagekit/nodejs';
 
+
 const client = new ImageKit({
-  privateKey: process.env['IMAGEKIT_PRIVATE_KEY'],
-  publicKey: process.env['IMAGEKIT_PUBLIC_KEY'],
-  urlEndpoint: process.env['IMAGEKIT_URL_ENDPOINT']
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
 });
 
-const response = await client.files.upload({
-  file: fs.createReadStream('path/to/file'),
-  fileName: 'file-name.jpg',
-});
 
-console.log(response);
 
 export default client;
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,4 @@
-import User from "../models/User";
+import User from "../models/User.js";
 import client from "../configs/imagesKit.js";
 import fs from "fs";
 
@@ -19,17 +19,21 @@ export const getUserData = async (req, res) => {
 }
 
 
+
 // Update User Data 
+
 export const updateUserData = async (req, res) => {
     try {
         const {userId} = await req.auth();
-        const {username, bio, location, full_name} = req.body;
+        let {username, bio, location, full_name} = req.body;
+
+
         const tempUser = await User.findById(userId);
 
         !username && (username = tempUser.username);
 
          if(tempUser.username !== username){
-            const user = User.findOne({username});
+            const user = await User.findOne({username});
             if(user){
                 //we will not change the username if it is already taken
                 username = tempUser.username;
@@ -38,13 +42,13 @@ export const updateUserData = async (req, res) => {
 
         const updateData ={
             username,
-            bio,
-            location,
-            full_name
+            bio: bio ?? tempUser.bio,
+            location: location ?? tempUser.location,
+            full_name: full_name ?? tempUser.full_name,
         };
 
-        const profile = req.files.profile && req.files.profile[0]
-        const cover = req.files.cover && req.files.cover[0]
+        const profile = req.files?.profile?.[0];
+        const cover = req.files?.cover?.[0];
 
         if(profile){
             const buffer = fs.readFileSync(profile.path);
@@ -150,3 +154,26 @@ export const followUser = async (req, res) => {
 }
 
 //Unfollow User
+
+export const unfollowUser = async (req, res) => {
+    try {
+        const {userId} = await req.auth();
+        const {id} = req.body;
+
+        const user = await User.findById(userId);
+
+       user.following = user.following.filter(user=> user !== id);
+       await user.save();
+
+       const toUser = await User.findById(id);
+       toUser.followers = toUser.followers.filter(user=> user !== userId);
+       await toUser.save();
+
+        res.json({success: true, message: 'You are no longer following this user'});
+
+  
+    } catch (error) {
+        console.error(error);
+        res.json({success: false, message: error.message});
+    }
+}
